ee755141350724307a2cba1cc2b062d8f2624c40
Oleksandr Chyrko <oleksandr@gigaspaces.com>
2015-02-06 13:11:34 +0200

    CFY-2115: Set DNS servers properly

--- vsphere_plugin_common/__init__.py
+++ vsphere_plugin_common/__init__.py
@@ -304,7 +304,7 @@ class ServerClient(VsphereClient):
 
             globalip = vim.vm.customization.GlobalIPSettings()
             if dns_servers:
-                globalip.dnsSuffixList = dns_servers
+                globalip.dnsServerList = dns_servers
             customspec.globalIPSettings = globalip
 
             clonespec.customization = customspec
