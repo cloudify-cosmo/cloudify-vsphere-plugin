f8d146fd67cae881efb50767f81b9d38b6b70783
limor-gs <limor@gigaspaces.com>
2016-02-25 09:23:51 +0200

    update version to 2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 setuptools.setup(
     zip_safe=True,
     name='cloudify-vsphere-plugin',
-    version='2.0.dev1',
+    version='2.0',
     packages=[
         'vsphere_plugin_common',
         'vsphere_server_plugin',
