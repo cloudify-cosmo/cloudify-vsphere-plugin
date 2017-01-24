c600012050dc893267c8ca13c75c838fec816b78
you@example.com <you@example.com>
2017-01-24 14:19:50 +0000

    vSphere plugin 2.2.0 update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 setuptools.setup(
     zip_safe=True,
     name='cloudify-vsphere-plugin',
-    version='2.1.0',
+    version='2.2.0',
     packages=[
         'vsphere_plugin_common',
         'vsphere_plugin_common.vendored_collections',
