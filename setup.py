c94e1b4a3057b8a0103ad800670a7f5c4ddf4f8f
geokala <walker_s@hotmail.co.uk>
2017-02-07 15:18:05 +0000

    2.2.1 release (#81)
    * VSPHERE-86-Handle VMs still being cloned
    
    * Bump version to 2.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 setuptools.setup(
     zip_safe=True,
     name='cloudify-vsphere-plugin',
-    version='2.2.0',
+    version='2.2.1',
     packages=[
         'vsphere_plugin_common',
         'vsphere_plugin_common.vendored_collections',
