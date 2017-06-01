f15f036b7f7e530701c89bf6a6c492216fd331ad
Matt Wheeler <m@funkyhat.org>
2017-06-01 18:48:57 +0100

    Pass the host cert to the client helper if it's set

--- system_tests/helpers/clients.py
+++ system_tests/helpers/clients.py
@@ -3,12 +3,14 @@ from vsphere_plugin_common import ServerClient
 
 def get_vsphere_client(tester_conf):
     client = ServerClient()
-    client.connect(
-        {
+    cfg = {
             'host': tester_conf['vsphere.host'],
             'username': tester_conf['vsphere.username'],
             'password': tester_conf['vsphere.password'],
             'port': tester_conf['vsphere.port'],
         }
-    )
+    path = tester_conf.get('vsphere.certificate_path')
+    if path:
+        cfg['certificate_path'] = path
+    client.connect(cfg)
     return client
