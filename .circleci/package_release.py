be6b0cec1660e01b435c163cefe3824b008f1a18
earthmant <trammell@cloudify.co>
2020-08-23 21:43:18 +0300

    dealing with issues __DOCS__: CY-1049-CYBL-1049-500, CY-1049-CYBL-1049-505, CY-1049-CYBL-1049-stuff

--- .circleci/package_release.py
+++ .circleci/package_release.py
@@ -3,11 +3,11 @@ from ecosystem_cicd_tools.release import plugin_release_with_latest
 from ecosystem_cicd_tools.validations import get_plugin_yaml_version
 
 
-setup_py = path.join(
+plugin_yaml = path.join(
     path.abspath(path.join(path.dirname(__file__), pardir)),
-    'setup.py')
+    'plugin.yaml')
 
 
 if __name__ == '__main__':
     plugin_release_with_latest(
-        'cloudify-vsphere-plugin', get_plugin_yaml_version(setup_py))
+        'cloudify-vsphere-plugin', get_plugin_yaml_version(plugin_yaml))
