857bc4c31ccac3146c4b584957f72c377abc1906
Ahmad Musa <ahmad@cloudify.co>
2023-09-21 13:39:09 +0300

    make-cpu-memory-hot-add-configurable

--- vsphere_plugin_common/clients/server.py
+++ vsphere_plugin_common/clients/server.py
@@ -544,6 +544,9 @@ class ServerClient(VsphereClient):
             clone_vm=None,
             disk_provision_type=None,
             disk_size=None,
+            cpu_hot_add=True,
+            cpu_hot_remove=True,
+            memory_hot_add=True,
             **_):
 
         self._logger.debug(
@@ -705,9 +708,9 @@ class ServerClient(VsphereClient):
         vmconf = vim.vm.ConfigSpec()
         vmconf.numCPUs = cpus
         vmconf.memoryMB = memory
-        vmconf.cpuHotAddEnabled = True
-        vmconf.memoryHotAddEnabled = True
-        vmconf.cpuHotRemoveEnabled = True
+        vmconf.cpuHotAddEnabled = cpu_hot_add
+        vmconf.memoryHotAddEnabled = memory_hot_add
+        vmconf.cpuHotRemoveEnabled = cpu_hot_remove
         vmconf.deviceChange = devices
 
         clonespec = vim.vm.CloneSpec()
